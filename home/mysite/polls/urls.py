18393769410199f11deffffacfec690d247ab4e9
Raphael Hassine <raphael.hassine@student.isae-supaero.fr>
2023-04-08 16:18:53 +0200

    Fixing path, workspace and app name for polls : everything's working fine including renders

--- home/mysite/polls/urls.py
+++ home/mysite/polls/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+from . import views
 from .views import index
 
+app_name = 'account'
+
 urlpatterns = [
-    path("", index, name="index"),
+    path("", views.index, name="index"),
+        
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+        
+    path("<int:question_id>/", views.detail, name="detail"),
+    
+    path("<int:question_id>/results/", views.results, name="results"),
+    
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
